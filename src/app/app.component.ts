7be461b857088fa48a135b34dab583b43f4df3e9
Mazher Hussain <mazherhussain926@gmail.com>
2025-04-21 08:44:59 +0500

    modify header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoaderService } from './shared/services/loader.service';
 import { LoaderComponent } from './shared/components/loader/loader.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styles: [],
 })
